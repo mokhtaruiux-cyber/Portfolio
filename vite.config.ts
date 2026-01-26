761dc307ee9485be9718a5db70c5ec229ce2aace
Mohammed Mokhtar <mokhtaruiux@gmail.com>
2026-01-26 12:58:10 +0200

    refactor(step 6.1): add vitest + rtl

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
@@ -12,5 +12,9 @@ export default defineConfig(() => ({
     alias: {
       '@': path.resolve(__dirname, '.'),
     }
-  }
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+  },
 }));
